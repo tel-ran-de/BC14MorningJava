e38f3a4aa99cd6ec3669aa0002c7fbdfbf3e70c3
andron13 <andron13@gmail.com>
2020-10-21 13:21:53 +0200

    day31 done

--- src/day32/Logic09_07.java
+++ src/day32/Logic09_07.java
@@ -8,8 +8,11 @@ public class Logic09_07 {
     }
 
     private static boolean closeFar(int a, int b, int c) {
-
-
-        return false;
+        int diff1, diff2, diff3;
+        diff1 = Math.abs(a - b);
+        diff2 = Math.abs(a - c);
+        diff3 = Math.abs(b - c);
+        return diff1 <= 1 && diff2 >= 2 && diff3 >= 2
+                || diff2 <= 1 && diff1 >= 2 && diff3 >= 2;
     }
 }
