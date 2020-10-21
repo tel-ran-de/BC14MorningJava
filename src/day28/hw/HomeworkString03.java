3e580668822b7b3adbcad9febba154870f47595d
Anastasiya S <nastua.matveeva@gmail.com>
2020-10-21 18:17:01 +0200

    day28 hw

---
+++ src/day28/hw/HomeworkString03.java
@@ -0,0 +1,26 @@
+package day28.hw;
+
+public class HomeworkString03 {
+    public static void main(String[] args) {
+        System.out.println(countYZ("fez day"));// →2
+        System.out.println(countYZ("day fez"));// →2
+        System.out.println(countYZ("day fyyyz"));// →2
+    }
+
+    private static int countYZ(String s1) {
+        char y = 'y';
+        char z = 'z';
+        char no = ' ';
+        int count = 0;
+        for (int i = 0; i < s1.length() - 1; i++) {
+            if (s1.charAt(i) == y && s1.charAt(i + 1) == no || s1.charAt(i) == z && s1.charAt(i + 1) == no) {
+                count++;
+            }
+
+        }
+        if (s1.charAt(s1.length() - 1) == y || s1.charAt(s1.length() - 1) == z) {
+            count++;
+        }
+        return count;
+    }
+}
