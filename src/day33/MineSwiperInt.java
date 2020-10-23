b567dc301f9d4fd3e754f39df9b8bdbf678fe246
AHaustov <anton.haust@gmail.com>
2020-10-23 13:26:22 +0200

    minesweeper

--- src/day33/MineSwiperInt.java
+++ src/day33/MineSwiperInt.java
@@ -17,33 +17,71 @@ public class MineSwiperInt {
         // 7. проверка ниличия бомб в клетках проверки
         int[][] field = createField(10);
         //System.out.println(Arrays.toString(field));
-        setNumber(field);
+        searchBomb(field);
         printArray(field);
 
     }
 
-    private static void setNumber(int[][] field) {
+    private static void searchBomb(int[][] field) {
         int bomb = -1;
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
                 if (field[i][j] == bomb) {
-                    searchHorizontaly(field, i, j);
+                    setNumber(field, i, j);
                 }
             }
 
         }
     }
 
-    private static void searchHorizontaly(int[][] arr, int x, int y) {
-        if (arr.length - 1 != x) {
-            arr[x][y - 1] = arr[x][y - 1] + 1;
+    private static void setNumber(int[][] arr, int x, int y) {
+        int bomb = -1;
+        if (y - 1 >= 0) {
+            if (arr[x][y - 1] != bomb) {
+                arr[x][y - 1]++;
+            }
+        }
+        if (y + 1 < arr.length) {
+            if (arr[x][y + 1] != bomb) {
+                arr[x][y + 1]++;
+            }
+        }
+        if (x - 1 >= 0) {
+            if (arr[x - 1][y] != bomb) {
+                arr[x - 1][y]++;
+            }
+            if (y - 1 >= 0) {
+                if (arr[x - 1][y - 1] != bomb) {
+                    arr[x - 1][y - 1]++;
+                }
+            }
+            if (y + 1 < arr.length) {
+                if (arr[x - 1][y + 1] != bomb) {
+                    arr[x - 1][y + 1]++;
+                }
+            }
+        }
+        if (x + 1 < arr.length) {
+            if (arr[x + 1][y] != bomb) {
+                arr[x + 1][y]++;
+            }
+            if (y - 1 >= 0) {
+                if (arr[x + 1][y - 1] != bomb) {
+                    arr[x + 1][y - 1]++;
+                }
+            }
+            if (y + 1 < arr.length) {
+                if (arr[x + 1][y + 1] != bomb) {
+                    arr[x + 1][y + 1]++;
+                }
+            }
         }
     }
 
     private static void printArray(int[][] field) {
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
-                System.out.print(field[i][j] + " ");
+                System.out.printf("%3d",field[i][j]);
             }
             System.out.println();
         }
